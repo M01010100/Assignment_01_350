fe4dc8f582880f997768c6adee415d32cd392c9c
Mike Nguyen <mikeynguyen29@gmail.com>
2024-09-21 00:36:05 -0400

    Progress on problem set 3, 6, 9. Finishing up

---
+++ Mike.sql
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+-- Question 3 Solution
+-- Author: Mike Nguyen 
+
+SELECT first_name, last_name
+FROM employees
+JOIN jobs
+ON employees.job_id = jobs.job_id
+WHERE jobs.job_title LIKE '%Manager%';
+
+
+
+
+-- Question 6 Solution
+-- Author: Mike Nguyen 
+
+SELECT AVG(e.salary) AS average_salary
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE d.department_name LIKE '%Public Relations';
+
+--Question 9
+-- Author: Mike Nguyen 
+select SUM(e.salary) AS total_salary
+from 
+employees e
+join 
+departments d ON e.department_id = d.department_id
+WHERE d.department_name LIKE '%Finance'
